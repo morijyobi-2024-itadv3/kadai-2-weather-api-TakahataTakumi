f483ee0ad0a6200a619c81085f20c0075f513ef2
takumiTakahata <t.takahata.sys22@morijyobi.ac.jp>
2024-06-27 12:15:00 +0900

    ver0.2のテストコードを作成

--- _tests_/route.test.ts
+++ _tests_/route.test.ts
@@ -87,5 +87,59 @@ describe("APIのレスポンス", () => {
         expect(response.headers.get("Content-Type")).toBe("application/json");
       });
     });
+
+    describe("レスポンスボディ", () => {
+      describe("jsonの構造", () => {
+        it("jsonに pref が含まれる", () => {
+          expect(data.pref).toBeDefined();
+          expect(data).toHaveProperty("pref");
+        });
+
+        it("jsonに area が含まれる", () => {
+          expect(data.area).toBeDefined();
+          expect(data).toHaveProperty("area");
+        });
+
+        it("jsonに today が含まれる", () => {
+          expect(data.today).toBeDefined();
+          expect(data).toHaveProperty("today");
+        });
+
+        it("jsonに today.weather が含まれる", () => {
+          expect(data.today.weather).toBeDefined();
+          expect(data.today).toHaveProperty("weather");
+        });
+
+        it("jsonに today.tempHigh が含まれる", () => {
+          expect(data.today.maxtemp).toBeDefined();
+          expect(data.today).toHaveProperty("tempHigh");
+        });
+
+        it("jsonに today.tempLow が含まれる", () => {
+          expect(data.today.mintemp).toBeDefined();
+          expect(data.today).toHaveProperty("tempLow");
+        });
+
+        it("jsonに tomorrow が含まれる", () => {
+          expect(data.tomorrow).toBeDefined();
+          expect(data).toHaveProperty("tomorrow");
+        });
+
+        it("jsonに tomorrow.weather が含まれる", () => {
+          expect(data.tomorrow.weather).toBeDefined();
+          expect(data.tomorrow).toHaveProperty("weather");
+        });
+
+        it("jsonに tomorrow.tempHigh が含まれる", () => {
+          expect(data.tomorrow.maxtemp).toBeDefined();
+          expect(data.tomorrow).toHaveProperty("tempHigh");
+        });
+
+        it("jsonに tomorrow.tempLow が含まれる", () => {
+          expect(data.tomorrow.mintemp).toBeDefined();
+          expect(data.tomorrow).toHaveProperty("tempLow");
+        });
+      });
+    });
   });
 });
